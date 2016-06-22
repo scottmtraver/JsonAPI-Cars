8cf2f325a2c589a1e962a42b6d6d00cc66f13039
Scott <scottmtraver@gmail.com>
2016-06-21 18:09:34 -0600

    Initial Commit

---
+++ Readme.md
@@ -0,0 +1,1 @@
+This is a FortuneJS NodeJS Server for the EmberJS SLC Meetup providing a demonstration endpoint for a car salesperson app.
---
+++ index.js
@@ -0,0 +1,58 @@
+const jsonApiSerializer = require('fortune-json-api')
+
+const fortune = require('fortune')
+
+const store = fortune({
+  car: {
+    year: { type: Number },
+    model: { type: String },
+    make: { type: String },
+    miles: { type: Number },
+    description: { type: String },
+
+    // Following and followers are inversely related (many-to-many).
+    seller: { link: 'seller', isArray: false }
+  },
+  seller: {
+    name: { type: String },
+    rating: { type: Number },
+
+    // One-to-many relationship of post author to user posts.
+    car: { link: 'car', isArray: true }
+  }
+});
+
+
+const http = require('http')
+
+// `instance` is an instance of Fortune.js.
+const listener = fortune.net.http(store, {
+  serializers: [
+    // The `options` object here is optional.
+    [ jsonApiSerializer, { prefix: 'api/' } ]
+  ]
+})
+// The listener function may be used as a standalone server, or
+// may be composed as part of a framework.
+const server = http.createServer(listener)
+
+server.listen(3000);
+
+//seed sellers and cars
+store.request({
+  type: 'seller',
+  method: 'create',
+  payload: [ { name: 'Bob', rating: 4 }, { name: 'Mary', rating: 5 } ]
+})
+
+store.request({
+  type: 'car',
+  method: 'create',
+  payload: [ {
+    year: 2015,
+    model: 'Model S',
+    make: 'Tesla',
+    miles: 1200,
+    description: 'Awesome car!'
+  }]
+})
